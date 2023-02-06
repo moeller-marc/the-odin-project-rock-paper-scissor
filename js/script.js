6a84440dbb2145c8e69ef77a14e7aaba53577788
moeller-marc <122219863+moeller-marc@users.noreply.github.com>
2023-02-06 16:38:59 +0100

    remove unnecessary function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-function getUserInput() {
-  let userChoice = prompt("what do you chose");
-  if (userChoice.toLowerCase() == "rock") {
-    let userChoice = 0;
-    return userChoice;
-  }
-  if (userChoice.toLowerCase() == "paper") {
-    let userChoice = 1;
-    return userChoice;
-  }
-  if (userChoice.toLowerCase() == "scissors") {
-    let userChoice = 2;
-    return userChoice;
-  }
-  return userChoice;
-}
-
 function playOneRound() {
   let userChoice = getUserInput();
   let computerChoice = Math.floor(Math.random() * 3);
