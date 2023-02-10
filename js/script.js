3ad98207878644fece3bc49de1a73e4bd9df50d3
moeller-marc <122219863+moeller-marc@users.noreply.github.com>
2023-02-10 16:09:46 +0100

    add function to display outcome message

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ const outcomeField = document.querySelector("#outcomeField");
 const buttonRock = document.querySelector("#rock");
 const buttonPaper = document.querySelector("#paper");
 const buttonScissors = document.querySelector("#scissors");
+
+function outcomeMessage(out) {
+  if (out == 0) {
+    outcome = "draw";
+  }
+  if (out == 1) {
+    outcome = "You win";
+    scorePlayer++;
+  }
+  if (out == 2) {
+    outcome = "You loose";
+    scoreComputer++;
+  }
+}
+
+function gameOutcome(scorePlayer, scoreComputer) {
+  if (scorePlayer == 5) {
+    console.log("You won the game");
+    scorePlayer = 0;
+    scoreComputer = 0;
+  }
+  if (scoreComputer == 5) {
+    console.log("You lost the game");
+    scorePlayer = 0;
+    scoreComputer = 0;
+  }
+}
